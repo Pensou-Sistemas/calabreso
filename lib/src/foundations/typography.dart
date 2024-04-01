6cb01ce9b1414bc4f8fded84f97456fb220cdf80
furtadino <igortfurtado@gmail.com>
2024-04-01 16:04:13 -0300

    changing fontWeight

--- lib/src/foundations/typography.dart
+++ lib/src/foundations/typography.dart
@@ -3,37 +3,37 @@ import 'package:flutter/material.dart';
 abstract class DSTypography {
   static TextStyle get headlineLarge => const TextStyle(
         fontSize: 32,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get headlineMedium => const TextStyle(
         fontSize: 28,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get headlineSmall => const TextStyle(
         fontSize: 24,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get titleLarge => const TextStyle(
         fontSize: 20,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get titleMedium => const TextStyle(
         fontSize: 18,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get titleSmall => const TextStyle(
         fontSize: 16,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get bodyLarge => const TextStyle(
         fontSize: 16,
-        fontWeight: FontWeight.w400,
+        fontWeight: FontWeight.w500,
       );
 
   static TextStyle get bodyMedium => const TextStyle(
@@ -43,21 +43,21 @@ abstract class DSTypography {
 
   static TextStyle get bodySmall => const TextStyle(
         fontSize: 12,
-        fontWeight: FontWeight.w400,
+        fontWeight: FontWeight.w500,
       );
 
   static TextStyle get labelLarge => const TextStyle(
         fontSize: 14,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
   static TextStyle get labelMedium => const TextStyle(
         fontSize: 12,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get labelSmall => const TextStyle(
         fontSize: 11,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w600,
       );
 
   static TextStyle get textButton => const TextStyle(
